6816c0618a158d9ae2c2007cae08f335ba2e9d8a
Trizzlenova <tdswayzee@gmail.com>
2021-04-12 18:35:32 -0700

    setting up view/template scaffolding

--- views/portfolio.py
+++ views/portfolio.py
@@ -4,6 +4,25 @@ portfolio = Blueprint('portfolio', __name__)
 
 
 @portfolio.route('/')
-def base():
-    palette = ['#01aff3', '#6bf700', '#eff109', '#f64e0c', '#cb268b']
-    return render_template('base.html', colors=palette)
+def main():
+    return render_template('main.html')
+
+
+@portfolio.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@portfolio.route('/skills')
+def skills():
+    return render_template('skills.html')
+
+
+@portfolio.route('/experience')
+def experience():
+    return render_template('experience.html')
+
+
+@portfolio.route('/contact')
+def contact():
+    return render_template('contact.html')
